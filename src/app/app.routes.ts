f905d21bbb9dd52d9251b96c36efd94080b32ac4
AdrianGaray <adriangaray396@gmail.com>
2026-02-25 19:22:40 -0300

    Capítulo 73 - Componente de Error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ListadoProductosComponent } from './listado-productos/listado-productos.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { ErrorComponent } from './error/error.component';
 
 export const routes: Routes = [
     {path:'', component: ListadoProductosComponent}, //localhost:4200/
@@ -13,7 +14,9 @@ export const routes: Routes = [
     // Ruta con parámetro /editar/:id
     // : indica parámetro dinámico.
     // Ejemplo: http://localhost:4200/editar/5
-    {path:'editar/:id', component: FormularioComponent }
+    {path:'editar/:id', component: FormularioComponent },
+    // Ruta comodin para cualquier otra ruta no registrada
+    {path: '**', component: ErrorComponent}
 ];
 
 
